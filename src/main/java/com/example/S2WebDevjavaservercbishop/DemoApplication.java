e53a229f3a10a194d8568d4792dcd002fcbca260
chasebish <bishop.c@husky.neu.edu>
2018-07-03 13:59:57 -0400

    init commit

---
+++ src/main/java/com/example/S2WebDevjavaservercbishop/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.S2WebDevjavaservercbishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
